771fb8e4e5f22d23013d4ee1e6d69f0c2f44ee67
Javier Martinez <javier.martinez@um.es>
2017-09-15 11:57:29 +0200

    bibtex added and build script edited

--- build_test.sh
+++ build_test.sh
@@ -1,4 +1,6 @@
+roxygen2::roxygenise()
 # symbolic link of extdata when documenting
 # sudo add-apt-repository ppa:anton+/photo-video-apps
-R CMD build --compact-vignettes='gs+qpdf' FlowRegEnvCost
+#R CMD build --compact-vignettes='gs+qpdf' FlowRegEnvCost
+R CMD build FlowRegEnvCost
 R CMD check --as-cran FlowRegEnvCost_0.1.0.tar.gz
