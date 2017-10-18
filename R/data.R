b08cd229a6a22bdd664ec8ea6268bca8d5edab42
Javier Martinez <javier.martinez@um.es>
2017-10-18 14:41:56 +0200

    version 0.1.1

--- R/data.R
+++ R/data.R
@@ -1,11 +1,12 @@
 #' River water flow data.
 #'
-#' A dataset containing daily river water flow data for the Esla river at the Riaño dam (Northern Spain) from 01/10/1964 to 30/09/2011.
+#' A dataset containing daily river water flow data for the Esla river at the Riaño dam (Northern Spain) from 01/10/1964 to 30/09/2011. The library adds missing days within the whole period automatically with NA flow values when you enter your own time series data.
 #'
 #' @format An example data frame with 17166 rows and 2 variables:
 #' \describe{
-#' \item{Date}{Date}
+#' \item{Date}{Date (dd/mm/yyyy)}
 #' \item{Flow}{Water flow, in m^{3}/s}
 #' }
-#' @source \url{https://doi.org/10.1007/s11269-017-1663-0}
+#' @source Source: \url{https://doi.org/10.1007/s11269-017-1663-0}
+
 "flowdata"
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # FlowRegEnvCost
-An application to calculate the daily environmental costs of flow regulation.
+An R library to calculate the daily environmental costs of flow regulation.
+
+The stable library can be fetch from CRAN at https://cran.r-project.org/web/packages/FlowRegEnvCost/index.html
